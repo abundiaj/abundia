af2ff48668e9c2b1e0dce537acc93527dffcb09c
abundiaj <yolynelfuentes@gmail.com>
2026-05-26 00:01:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const sheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSHLrhQjaxFPEO
 
 let productos = [];
 let carrito = [];
-let categoriaActual = "dia del padre"; 
+let categoriaActual = "dia-del-padre"; 
 
 const productList = document.getElementById("product-list");
 const cart = document.getElementById("cart");
